292ca6ca38b38aade7c78cfcb7844752161d112a
Yifan Peng <yfpeng@udel.edu>
2017-01-02 00:00:25 -0500

    bugs: add source package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.dev27'
+VERSION = '1.0.dev28'
 
 setup(
         version=VERSION,
-        packages=find_packages('bioc', exclude=["tests.*", "tests"]),
+        packages=find_packages(exclude=["tests.*", "tests"]),
         install_requires=[
             'docutils>=0.3',
             'lxml'],
