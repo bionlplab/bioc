88b5bc8827fb760d69fe3ca1354ef1e865e86879
Yifan Peng <yifan.peng@nih.gov>
2017-05-16 09:58:38 -0400

    tests for get_text

---
+++ tests/bioc/test_utils.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+
+from ..context import bioc
+
+
+def test_get_text():
+    filename = os.path.join(os.path.dirname(__file__), 'everything.xml')
+    with open(filename) as fp:
+        collection = bioc.load(fp)
+
+    assert (0, 'abcdefghijklmnopqrstuvwxyz') == bioc.get_text(collection.documents[0])
+    assert (27, 'abcdefghijklm') == bioc.get_text(collection.documents[1].passages[0])
+    assert (0, '\n'*27 + 'abcdefghijklm') == bioc.get_text(collection.documents[1])
+
+    with pytest.raises(ValueError):
+        next(bioc.get_text('Foo'))
