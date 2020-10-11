371b6ec1e5f663de78c2c69839f91069ba88f64a
Yifan Peng <pengyifan.mail@gmail.com>
2020-10-11 11:25:57 -0400

    Update __init__.py

--- bioc/biocxml/__init__.py
+++ bioc/biocxml/__init__.py
@@ -3,3 +3,6 @@ BioC XML encoder and decoder
 """
 from bioc.biocxml.encoder import dump, dumps, BioCXMLDocumentWriter
 from bioc.biocxml.decoder import load, loads, BioCXMLDocumentReader
+
+__all__ = ['BioCXMLDocumentReader', 'BioCXMLDocumentWriter',
+           'dumps', 'dump', 'load', 'loads']
